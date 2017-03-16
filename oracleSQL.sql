ff7c7098a8569b761e3ca501017df4f1d48be73f
Pulkit219 <ascension1110@gmail.com>
2017-03-16 18:02:34 -0400

    3rd

--- oracleSQL.sql
+++ oracleSQL.sql
@@ -2,4 +2,6 @@ SHOW user;
 SELECT * FROM EMPLOYEES;
 SELECT employee_id,  last_name FROM employees;
 SELECT last_name, salary, salary +1000 FROM employees;
+SELECT first_name||' '||last_name FROM EMPLOYEES  --concatination operator--
+
 
