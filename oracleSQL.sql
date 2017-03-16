d08469ea1f19012d8c5799c939871b1896fab6ca
Pulkit219 <ascension1110@gmail.com>
2017-03-16 18:13:47 -0400

    4TH

--- oracleSQL.sql
+++ oracleSQL.sql
@@ -2,6 +2,8 @@ SHOW user;
 SELECT * FROM EMPLOYEES;
 SELECT employee_id,  last_name FROM employees;
 SELECT last_name, salary, salary +1000 FROM employees;
-SELECT first_name||' '||last_name FROM EMPLOYEES  --concatination operator--
-
-
+SELECT first_name||' '||last_name FROM EMPLOYEES;  --concatination operator--
+SELECT FIRST_NAME, SALARY, HIRE_DATE FROM EMPLOYEES
+WHERE FIRST_NAME='Neena';
+SELECT FIRST_NAME, SALARY, HIRE_DATE FROM EMPLOYEES
+WHERE HIRE_DATE >'31-DEC-05';
